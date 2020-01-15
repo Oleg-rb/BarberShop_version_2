cfbbfb46cc0634af2adb107369fdfe72ce378ddb
Oleg-rb <oleg1107@rambler.ru>
2020-01-15 10:57:53 +0300

    Add Client entity

--- BarberShopHQ.rb
+++ BarberShopHQ.rb
@@ -2,6 +2,12 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:barbershop.db"
+
+class Client < ActiveRecord::Base
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/Oleg-rb/Template/\">Original</a> pattern has been modified for <a href=\"https://github.com/Oleg-rb/Template/\">Template</a>"			
