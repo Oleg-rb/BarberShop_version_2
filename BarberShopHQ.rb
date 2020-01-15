d07ec76faa75e273426d82acc6f5d42c637c97dc
Oleg-rb <oleg1107@rambler.ru>
2020-01-15 11:54:00 +0300

    Add Barbers migration

--- BarberShopHQ.rb
+++ BarberShopHQ.rb
@@ -9,6 +9,9 @@ set :database, "sqlite3:barbershop.db"
 class Client < ActiveRecord::Base
 end
 
+class Barber < ActiveRecord::Base
+end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/Oleg-rb/Template/\">Original</a> pattern has been modified for <a href=\"https://github.com/Oleg-rb/Template/\">Template</a>"			
 end
